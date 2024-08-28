fa837bfef9802156ddc4cdb00d74534528ac2fa4
hayasin2004 <masataka1kousuke1@gmail.com>
2024-08-28 12:34:16 +0900

    Toppage移植完了しました! http://localhost:3000/pages/toppageをserver起動してからブラウザ調べるとできます。 軽くURLの仕組みについて触れるとNextではpagesディレクトリの中にディレクトリを作ると自動的にルーティングが完成します。なので今回の場合はpagesディレクトリの中にtoppageディレクトリを作っているので自動的に上記のURLになります。尚pages/toppage/○○ の○○のファイル名称は必ずpage.tsxである必要があるので実際に作るときはそのことを意識してください！！

---
+++ src/app/_components/sidebar/Sidebar.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import "./Sidebar.css"
+import Image from  "next/image"
+const Sidebar = () => {
+
+    return (
+        <div className="introduction">
+
+            <div className="sidebar">
+                <div className="cart">
+                    <h1>
+                        Cart
+                    </h1>
+                    <p>
+                        ○
+                    </p>
+                </div>
+                <hr id="side_line"/>
+
+                <div className="cart_inProduct">
+                    <figure>
+                        <Image src="/images/pic_car.jpg" width={200} height={200} alt="商品の写真" />
+                    </figure>
+                    <ul>
+                        <li>ワンピース</li>
+                        <li>出品者:〇〇〇〇</li>
+                        <li>価格:〇〇〇〇</li>
+                    </ul>
+                </div>
+                <p id="total_price">
+                    合計金額
+                </p>
+                <p id="total_yen">
+                    〇〇〇〇円
+                </p>
+                <a href="#" id="side_product_detail">
+                    商品ページ
+                </a>
+
+
+            </div>
+
+        </div>
+    );
+}
+
+
+export default Sidebar;
---
+++ src/app/_components/toppageMain/ToppageMain.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./toppageMain.css"
+
+const ToppageMain = () => {
+
+    return (
+        <>
+            <div >
+                <div className={"top_main"}>
+
+                    <div className="slideshow">
+                        <p>スライドショー</p>
+                    </div>
+
+                </div>
+                <hr id="slideshow_line"/>
+
+                <div className="new_Prodct">
+
+                    <h2>
+                        New Arrival 新着
+                    </h2>
+                </div>
+            </div>
+        </>
+    );
+}
+
+
+export default ToppageMain;
