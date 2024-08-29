456be800f6b5d4e23a1fa74a6256ab86c841dc31
hayasin2004 <masataka1kousuke1@gmail.com>
2024-08-29 20:41:24 +0900

    購入履歴確認ページを作成しました。 即興でさくせいなのでレイアウト雑いです！

---
+++ src/app/pages/paidNote/page.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import Header from "@/app/_components/header/Header";
+import Image from "next/image";
+import "./paidNote.css"
+
+const PaidNote = () => {
+
+    return (
+        <>
+            <Header/>
+            <div className={"paidText"}>
+                <h2>
+                    あなたの購入履歴
+                </h2>
+            </div>
+            <div className={"listing"}>
+                <div className={"listing_product"}>
+                    <div className={"product"}>
+                        <Image src={"/images/sample01.jpg"} width={200} height={200} alt={"購入履歴"}/>
+                        <div className={"product_text"}>
+                            <p>
+                                商品名
+                            </p>
+                            <p>出品者 : xxxx</p>
+                            <p>価格 : xxxx</p>
+                        </div>
+                    </div>
+                    <div className={"listingText"}>
+                        <h3>評価</h3>
+                        <p>adfjdafjapdofjadjfpoadjfpoajdfpoadjfpasd;lfaslfjads;lfjasdlkfjadsjf;</p>
+                    </div>
+                    <div className={"product"}>
+                        <Image src={"/images/sample01.jpg"} width={200} height={200} alt={"購入履歴"}/>
+                        <div className={"product_text"}>
+                            <p>
+                                商品名
+                            </p>
+                            <p>出品者 : xxxx</p>
+                            <p>価格 : xxxx</p>
+                        </div>
+                    </div>
+                    <div className={"listingText"}>
+                        <h3>評価</h3>
+                        <p>adfjdafjapdofjadjfpoadjfpoajdfpoadjfpasd;lfaslfjads;lfjasdlkfjadsjf;</p>
+                    </div>
+                </div>
+            </div>
+            <div className={"listing"}>
+                <div className={"listing_product"}>
+                    <div className={"product"}>
+                        <Image src={"/images/sample01.jpg"} width={200} height={200} alt={"購入履歴"}/>
+                        <div className={"product_text"}>
+                            <p>
+                                商品名
+                            </p>
+                            <p>出品者 : xxxx</p>
+                            <p>価格 : xxxx</p>
+                        </div>
+                    </div>
+                    <div className={"listingText"}>
+                        <h3>評価</h3>
+                        <p>adfjdafjapdofjadjfpoadjfpoajdfpoadjfpasd;lfaslfjads;lfjasdlkfjadsjf;</p>
+                    </div>
+                    <div className={"product"}>
+                        <Image src={"/images/sample01.jpg"} width={200} height={200} alt={"購入履歴"}/>
+                        <div className={"product_text"}>
+                            <p>
+                                商品名
+                            </p>
+                            <p>出品者 : xxxx</p>
+                            <p>価格 : xxxx</p>
+                        </div>
+                    </div>
+                    <div className={"listingText"}>
+                        <h3>評価</h3>
+                        <p>adfjdafjapdofjadjfpoadjfpoajdfpoadjfpasd;lfaslfjads;lfjasdlkfjadsjf;</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+
+export default PaidNote;
