85606c694008dbbe9aa30e1ceabd348cf99a62e8
Masat_2003 <masataka1kousuke1@gmail.com>
2024-09-06 10:41:15 +0900

    あとはログインに飛ばすだけ

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -8,29 +8,30 @@ import {Slide} from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import {useSession, signIn, signOut} from "next-auth/react"
 import axios from "axios";
-import {useRouter} from "next/navigation";
+import {redirect, useRouter} from "next/navigation";
 import {Simulate} from "react-dom/test-utils";
 import error = Simulate.error;
 import createUser from "@/app/utils/registerUser";
 import Login from '../login/page';
-import  {useHistory} from  "react-router-dom"
+import {useNavigate} from "react-router";
 
 
 const Register = () => {
-    const {data: session, status} = useSession();
-    const dateAll = [session?.user.name, session?.user.email, session?.user.image]
-
-    if (status === "loading") {
-        console.log("123456789128912345678")
-    }
-    console.log("これはsessionです" + dateAll)
-    // ログインしたら自動的にトップページに飛ばされる
-    const handleGithubLogin = () => {
-        signIn("github", {callbackUrl: "/login"})
-    }
-    const handleGooleLogin = () => {
-        signIn("google", {callbackUrl: "/toppage"})
-    }
+    // {
+    //     const {data: session, status} = useSession();
+    //     const dateAll = [session?.user.name, session?.user.email, session?.user.image]
+    //
+    //     if (status === "loading") {
+    //         console.log("123456789128912345678")
+    //     }
+    //     console.log("これはsessionです" + dateAll)
+    //     // ログインしたら自動的にトップページに飛ばされる
+    //     const handleGithubLogin = () => {
+    //         signIn("github", {callbackUrl: "/login"})
+    //     }
+    //     const handleGooleLogin = () => {
+    //         signIn("google", {callbackUrl: "/toppage"})
+    // }
     // const handleFacebookLogin = () => {
     //     signIn("facebook" , {callbackUrl : "/toppage"})
     //     console.log(handleFacebookLogin)
@@ -38,7 +39,27 @@ const Register = () => {
     // const handleInstagramLogin = () => {
     //     signIn("instagram" , {callbackUrl : "/toppage"})
     // }
+    // }
+
+    // フォームがもしすべて入力されていたら次のページ
+    const [formValue, setFormValue]
+        = useState({UserName :"" , Email : "" , Password : "" , ConfirmPassword : ""})
+        console.log(formValue)
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        const {name , value} = e.target;
+        setFormValue({...formValue, [name]: value});
+    }
+    const handleSubmit = (e :  React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(formValue.UserName && formValue.Email && formValue.Password && formValue.ConfirmPassword ){
+            
+        }else{
+            window.alert("すべてのフォームを入力の上再度送信してください。")
+        }
+    }
     return (
+
 
         <>
             <header>
@@ -60,7 +81,7 @@ const Register = () => {
                     <div id="bgwhite">
                         <div id="form">
                             <h2>ユーザー登録</h2><br/>
-                            <form action={async (data: FormData) => {
+                            <form onSubmit={handleSubmit}  action={async (data: FormData) => {
                                 const username = data.get("userName") as string
                                 const email = data.get("Email") as string
                                 const password = data.get("Password") as string
@@ -68,18 +89,18 @@ const Register = () => {
                             }}>
                                 <label htmlFor="UserName">ユーザー名</label><br/>
 
-                                <input type="text" name="userName" id="UserName" required
+                                <input type="text" name="UserName" id="UserName" required onChange={onChange} value={formValue.UserName}
                                        placeholder="Enter your UserName"/><br/>
                                 <label htmlFor="Email">Email</label><br/>
-                                <input type="text" name="Email" id="Email" required
-                                       placeholder="Enter your E-mail Address"/><br/>
+                                <input type="text" name="Email" id="Email"  required value={formValue.Email}
+                                       placeholder="Enter your E-mail Address" onChange={onChange}/><br/>
 
-                                <input required type="password" name="Password" id="Password"
-                                       placeholder="Enter Password"/><br/>
+                                <input required type="password" name="Password" id="Password" value={formValue.Password}
+                                       placeholder="Enter Password" onChange={onChange}/><br/>
 
                                 <label htmlFor="PWCheck">パスワード(再入力)</label><br/>
-                                <input type="password" required name="PWCheck" id="PWCheck"
-                                       placeholder="Enter Password again "/><br/>
+                                <input type="password" required name="ConfirmPassword" id="PWCheck" value={formValue.ConfirmPassword}
+                                       placeholder="Enter Password again " onChange={onChange}/><br/>
 
                                 <button type="submit">
                                         ユ―ザーを作成
