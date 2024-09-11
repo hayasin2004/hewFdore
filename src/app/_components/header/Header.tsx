7d4be9e64f6b2d13349e6d0d490cba7bf163b773
hayasin2004 <masataka1kousuke1@gmail.com>
2024-09-12 05:29:29 +0900

    ログインしていないときは初期ユーザーアイコンをログインという文字をログインしているときは青色のユーザーアイコンとユーザーネームを表示するようにしました。

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
     console.log(user?.username)
 
 
-        // 通知用モーダル
+    // 通知用モーダル
     interface TabPanelProps {
         children?: React.ReactNode;
         index: number;
@@ -98,7 +98,7 @@ const Header = () => {
                             <li>
                                 <Link href={"listingScreen"}>
 
-                                出品
+                                    出品
                                 </Link>
                             </li>
                             <span id="short_line"><br/>
@@ -129,12 +129,19 @@ const Header = () => {
 
                             <li>
 
+                                {user ? <UserNavigationModal/> :
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"
+                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                         stroke-linejoin="round" className="lucide lucide-circle-user-round">
+                                        <path d="M18 20a6 6 0 0 0-12 0"/>
+                                        <circle cx="12" cy="10" r="4"/>
+                                        <circle cx="12" cy="12" r="10"/>
+                                    </svg>}
 
-                                    <UserNavigationModal />
 
                             </li>
                             <li>
-                                {user?.username}
+                                {user ? user.username : <p>ログイン</p>}
                             </li>
                             <li>
                                 <div>
