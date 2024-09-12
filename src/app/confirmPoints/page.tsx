d6a273c53a479a64a99fe94403d4cd14c6265d12
35018 <nhs35018@nhs.hal.ac.jp>
2024-09-12 15:21:48 +0900

    スライド画像用フォルダ

--- src/app/confirmPoints/page.tsx
+++ src/app/confirmPoints/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from "@/app/_components/header/Header";
 import "./confirmPoints.css"
 import Image from "next/image"
-
 const ConfirmUserProfile = () => {
 
     return (
