f88b614c7057b18cbb45f1b66ea9fd41479748de
35018 <nhs35018@nhs.hal.ac.jp>
2024-09-13 08:59:10 +0900

    試しにスライドの差し替えしてみましたが縦横比が合わないみたいです スライドか画像どっちかの比率を変えます

--- src/app/_components/toppageslideshow/Slideshow.tsx
+++ src/app/_components/toppageslideshow/Slideshow.tsx
@@ -7,7 +7,7 @@ import Box from "@mui/material/Box";
 
 const Slideshow = () => {
     const images = [
-        "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+        "images/slide/slide_perfume.png",
         "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
         "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
     ];
