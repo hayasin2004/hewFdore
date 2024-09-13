3f34d932a9d0dc7c864f9e3375246f9db76e53e4
35018 <nhs35018@nhs.hal.ac.jp>
2024-09-13 10:35:12 +0900

    sendAdressレイアウト調整

--- src/app/sendAddress/page.tsx
+++ src/app/sendAddress/page.tsx
@@ -48,30 +48,30 @@ const SendAddress = () => {
                         <label htmlFor="write">記入する</label>
                     </div>
                     <div className="Name">
-                        <label htmlFor="FamilyName"> 姓 </label><label htmlFor="FirstName">名</label>
+                        <label htmlFor="FamilyName"> 姓 </label><label htmlFor="FirstName" className={"threespace"}>名</label>
                         <input type="text" name="FamilyName" id="FamilyName" placeholder="苗字"/>
                         <input type="text" name="FirstName" id="FirstName" placeholder="名前"/>
                     </div>
                     <div className="Name">
-                        <label htmlFor="FamilyName_kana">フリ</label><label htmlFor="FirstName_kana">ガナ</label>
+                        <label htmlFor="FamilyName_kana">フリ</label><label htmlFor="FirstName_kana" className={"twospace"}>ガナ</label>
                         <input type="text" name="FamilyName" id="FamilyName_kana" placeholder="ミョウジ"/>
                         <input type="text" name="FirstName" id="FirstName_kana" placeholder="ナマエ"/>
                     </div>
-                    <p>住所</p>
+                    <p className={"middletext"}>住所</p>
                     <div>
-                        <label htmlFor="postcode">郵便番号</label>
+                        <label htmlFor="postcode" className={"onespace"}>郵便番号</label>
                         <input type="text" name="postcode" id="postcode" placeholder="ハイフンなし　半角"/>
                         <button type="button" id="Post">記入</button>
                         <div id="comment"></div>
                         <br/>
-                        <label htmlFor="prefecture">都道府県</label>
+                        <label htmlFor="prefecture" className={"onespace"}>都道府県</label>
                         <input type="text" name="prefecture" id="prefecture"/> <br/>
-                        <label htmlFor="city">市区町村</label>
+                        <label htmlFor="city" className={"onespace"}>市区町村</label>
                         <input type="text" name="city" id="city"/> <br/>
-                        <label htmlFor="number"> 番 地</label>
+                        <label htmlFor="number" className={"threespace"}> 番 地</label>
                         <input type="text" name="number" id="number"/>
                     </div>
-                    <p>緊急連絡先</p>
+                    <p className={"middletext"}>緊急連絡先</p>
                     <div>
                         <label htmlFor="telnumber">電話番号</label> <br/>
                         <input type="tel" name="telnumber" id="telnumber" placeholder="ハイフンなし　半角"/> <br/>
