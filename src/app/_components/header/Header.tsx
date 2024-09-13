dbe69010eae34321c76ead79b55a5ebaca6b1a77
35018 <nhs35018@nhs.hal.ac.jp>
2024-09-13 13:09:33 +0900

    ヘッダー調整

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -107,7 +107,7 @@ const Header = () => {
                     </span>
                             <Link href={"/searchResult"}>
 
-                                <li>
+                                <li id={"search"}>
                                     Search <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                                                 stroke-linecap="round" stroke-linejoin="round"
@@ -139,7 +139,7 @@ const Header = () => {
 
 
                             </li>
-                            <li>
+                            <li id={"name"}>
                                 {user ? user.username : <Link href={"login"}><p>ログイン</p></Link>}
                             </li>
                             <li>
