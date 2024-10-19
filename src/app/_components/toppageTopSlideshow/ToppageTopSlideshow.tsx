d3cc0a4fcb958cb1e2bd90b18db9208b11f94630
Masat_2003 <masataka1kousuke1@gmail.com>
2024-10-19 20:19:36 +0900

    ブログ説明、画像のスライドショーを追加しました。 参考サイトに似せるためブログの説明を1230ミリ秒として遅延を発生してスクロ―ルするようにしてます。

--- src/app/_components/toppageTopSlideshow/ToppageTopSlideshow.tsx
+++ src/app/_components/toppageTopSlideshow/ToppageTopSlideshow.tsx
@@ -1,28 +1,73 @@
 "use client"
 import React from 'react';
-import { Slide } from 'react-slideshow-image';
+import {Slide} from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import Images from "next/image";
+import "./toppageTopSlideshow.css"
+
+// スライドショーを二つに分けます。
+// 右のスライドショーはmap関数でtitle,imageを取得
+// 右のスライドショーはmap関数でid + 1で取り出して、title,imageを取得　→　次の表示。
+// 次へのボタン、戻るのボタンで二つを共有
 const ToppageTopSlideshow = () => {
     return (
         <>
-            <div>
 
-                <Slide slidesToScroll={2} slidesToShow={2} indicators={true}>
-                    <div className="each-slide-effect first_slide">
-                        <div>
-                            <Images className={"slideImage"} src={"/images/Slide/slide_color.png"} width={800}
-                                    height={500} alt={"スライドショー"}/>
+                <div className={"toppage_top_slideshow_left"}>
+                    {/*テキストのスライドショー*/}
+                    <div className={"left_slide_Text_position"}>
+                    <Slide slidesToScroll={1} slidesToShow={1} transitionDuration={1230} infinite  indicators={true} arrows={false}>
+                        <div className="each-slide-effect toppage_top_slideshow_slide_Text">
+                            <div>
+                                <h1>テスト１</h1>
+                                <p>これはテスト１</p>
+                            </div>　
                         </div>
+                        <div className="each-slide-effect toppage_top_slideshow_slide_Text">　
+                            <div>
+                                <h1>テスト2</h1>
+                                <p>これはテスト2</p>
+                            </div>
+                        </div>
+                    </Slide>
                     </div>
-                    <div className="each-slide-effect second_slide">
-                        <div>
-                            <Images className={"slideImage"} src={"/images/Slide/slide_perfume.png"} width={800}
-                                    height={500} alt={"スライドショー"}/>
+                    {/*右側の画像スライドショー*/}
+            <div className={"toppage_top_slideshow"}>
+                    <div className={"left_slide_position"}>
+                    <Slide slidesToScroll={1} slidesToShow={1} transitionDuration={1000} infinite indicators={true}>
+                        <div className="each-slide-effect toppage_top_slideshow_slide">
+                            <div>
+                                <Images className={"slide_left_Image"} src={"/images/Slide/slide_color.png"} width={600}
+                                        height={350} alt={"スライドショー"}/>
+                            </div>
+                        </div>
+                        <div className="each-slide-effect toppage_top_slideshow_slide">
+                            <div>
+                                <Images className={"slide_left_Image"} src={"/images/Slide/slide_perfume.png"} width={600}
+                                        height={350} alt={"スライドショー"}/>
 
+                            </div>
                         </div>
+                    </Slide>
                     </div>
-                </Slide>
+                </div>
+                {/*<div className={"toppage_top_slideshow_right"}>*/}
+                {/*    <Slide slidesToScroll={1} slidesToShow={1} infinite indicators={true}>*/}
+                {/*        <div className="each-slide-effect toppage_top_slideshow_first_slide">*/}
+                {/*            <div>*/}
+                {/*                <Images className={"slideImage"} src={"/images/Slide/slide_color.png"} width={400}*/}
+                {/*                        height={300} alt={"スライドショー"}/>*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*        <div className="each-slide-effect toppage_top_slideshow_second_slide">*/}
+                {/*            <div>*/}
+                {/*                <Images className={"slideImage"} src={"/images/Slide/slide_perfume.png"} width={400}*/}
+                {/*                        height={300} alt={"スライドショー"}/>*/}
+
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    </Slide>*/}
+                {/*</div>*/}
             </div>
         </>
     );
