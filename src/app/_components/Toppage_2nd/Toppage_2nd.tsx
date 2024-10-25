1a51c9658b603e96bb8ecd5e387ed5b9d3649192
hayasin2004 <masataka1kousuke1@gmail.com>
2024-10-25 17:46:47 +0900

    現時点での課題点 １，次のスライドを表示するボタンを押すときに反応するアニメーションが一番右の写真だけ。 →修正後は三つのスライドに同じ向きのアニメーションを付ける。 2,一個前に戻るときのボタンを押したときに流れるアニメーションが次のスライドに行くときのアニメーション。 →修正後は左から右に流れるアニメーション ３,画像サイズがバグ →修正後は枠内に綺麗に収まるようにCSSを当ててほしい 4,三つのスライドショーを動かすとき一緒に動いている →修正後は右から順に動いてほしい。
    作業するうえで時間が掛かってしまう所
    三つのスライドにそれぞれ遅延を発生させるところ。
    →スライドにつき新しい関数を作成する必要がある。
    →setTimeoutを酷使する必要がある。
    nextanimateのクラスは現在二個使われているが、あと一つ追加したいところ。
    prevanimateのクラスが今使われてるところが一つしかないから、他のところにも適用させる必要がある。

--- src/app/_components/Toppage_2nd/Toppage_2nd.tsx
+++ src/app/_components/Toppage_2nd/Toppage_2nd.tsx
@@ -1,39 +1,39 @@
-"use client"
-import React, {useState} from 'react';
-import "./common.css"
-
-// import {Swiper,SwiperSlide} from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import {Navigation} from "swiper/modules";
-
-import Product from "@/app/product/page";
-import Image from "next/image"
-import Link from "next/link";
-
-
-const Toppage_2nd = () => {
-
-    return (
-        <>
-            <div className={"inner"}>
-                {/*<Swiper*/}
-                {/*    navigation={true}*/}
-                {/*    modules={[Navigation]}*/}
-                {/*    spaceBetween={1}*/}
-                {/*    centeredSlides={true}*/}
-                {/*    slidesPerView={1.6}*/}
-                {/*    effect={"coverflow"}*/}
-
-                {/*>*/}
-                {/*    <SwiperSlide><div id={"test"}>スライド1</div></SwiperSlide>*/}
-                {/*    <SwiperSlide><div id={"test"}>スライド2</div></SwiperSlide>*/}
-                {/*    <SwiperSlide><div id={"test"}>スライド3</div></SwiperSlide>*/}
-                {/*</Swiper>*/}
-            </div>
-
-        </>
-    );
-};
-
-export default Toppage_2nd;
+// "use client"
+// import React, {useState} from 'react';
+// import "./common.css"
+// //
+// // import {Swiper,SwiperSlide} from "swiper/react";
+// // import 'swiper/css';
+// // import 'swiper/css/navigation';
+// // import {Navigation} from "swiper/modules";
+//
+// import Product from "@/app/product/page";
+// import Image from "next/image"
+// import Link from "next/link";
+//
+//
+// const Toppage_2nd = () => {
+//
+//     return (
+//         <>
+//             <div className={"inner"}>
+//                 <Swiper
+//                     navigation={true}
+//                     modules={[Navigation]}
+//                     spaceBetween={1}
+//                     centeredSlides={true}
+//                     slidesPerView={1.6}
+//                     effect={"coverflow"}
+//
+//                 >
+//                     <SwiperSlide><div id={"test"}>スライド1</div></SwiperSlide>
+//                     <SwiperSlide><div id={"test"}>スライド2</div></SwiperSlide>
+//                     <SwiperSlide><div id={"test"}>スライド3</div></SwiperSlide>
+//                 </Swiper>
+//             </div>
+//
+//         </>
+//     );
+// };
+//
+// export default Toppage_2nd;
