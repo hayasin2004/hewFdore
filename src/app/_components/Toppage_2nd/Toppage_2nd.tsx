215ee7d915ec5c40c2451d4a0a3ddcc5146c019e
35018 <nhs35018@nhs.hal.ac.jp>
2024-10-25 15:02:27 +0900

    コンポーネント作っただけ

---
+++ src/app/_components/Toppage_2nd/Toppage_2nd.tsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import "./common.css"
+import Product from "@/app/product/page";
+import Image from "next/image"
+import Link from "next/link";
+
+
+const Toppage_2nd = () => {
+
+    return (
+        <>
+            <div className={"inner"}>
+
+                    suraido
+            </div>
+
+        </>
+    );
+};
+
+export default Toppage_2nd;
