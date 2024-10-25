3d410b2e049c1d66f05578a7b9fbc0d9e7d2e890
hayasin2004 <masataka1kousuke1@gmail.com>
2024-10-25 14:27:59 +0900

    CSSアニメーションの一例

--- src/app/_components/toppage_top_slideshow/Toppage_top_slideshow.tsx
+++ src/app/_components/toppage_top_slideshow/Toppage_top_slideshow.tsx
@@ -24,19 +24,26 @@ const dummyData_slide_map_item: dummy[] = dummyData.map((item, index) => {
 
 const ToppageTopSlideshow: React.FC<dummy> = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
-
+    const [animate, setAnimate] = useState(false);
 
     // 配列順に画像を取り出してcurrentSlideにセットしている, テキストも同じ仕組み
     const goToNextSlide = () => {
         setCurrentSlide((prevSlide) => (prevSlide + 1) % dummyData_slide_map_item.length);
         // setCurrentText((prevSlide) => (prevSlide + 1));
         // console.log(currentText)
+        setAnimate(true);
+        setTimeout(() => {
+            setAnimate(false);
+            console.log("オンになったああああああああああああああああああああああああああああああああああああああああああああ")
+        }, 500);
     };
 
     // ひとつ前の画像を取り出してcurrentSlideにセットしている , テキストも同じ仕組み
     const goToPrevSlide = () => {
         // setCurrentText((prevText) => (prevText - 1 + dummyData_slide_map_item.length) % dummyData_slide_map_item.length);
         setCurrentSlide((prevSlide) => (prevSlide - 1 + dummyData_slide_map_item.length) % dummyData_slide_map_item.length);
+        console.log("kkokokokokokokokokokokokokokokokkoo")
+
     };
     // 長い計算の解説。
     // prevSlide - 1 + dummyData_slide_map_item.length は、負のインデックスを防ぐための計算。配列の長さを足すことで、負の値にならないようにしている。
@@ -46,7 +53,8 @@ const ToppageTopSlideshow: React.FC<dummy> = () => {
 
     return (
         <>
-            <div className={"slide_master"}>
+            <div className={animate ? 'animate' : ''}>
+                {/*cssアニメーション*/}
 
                 <div className="slide-show">
                     <div className="photo_list">
@@ -64,7 +72,7 @@ const ToppageTopSlideshow: React.FC<dummy> = () => {
                         />
                     </div>
                     <button onClick={goToPrevSlide}>Prev</button>
-                    <button onClick={goToNextSlide}>Next</button>
+                    <button 　onClick={goToNextSlide}>Next</button>
                 </div>
             </div>
         </>
