ae39fc0f0c735668a3d0c40490b40d683e9e81a2
35018 <nhs35018@nhs.hal.ac.jp>
2024-11-01 14:15:40 +0900

    Swiper 下にスライダー追加

--- src/app/_components/Toppage_2nd/Toppage_2nd.tsx
+++ src/app/_components/Toppage_2nd/Toppage_2nd.tsx
@@ -3,14 +3,16 @@ import React, {useState} from 'react';
 import "./common.css"
 
 // react-slideshow-image使う場合
-import {Slide} from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+// import {Slide} from 'react-slideshow-image';
+// import 'react-slideshow-image/dist/styles.css'
 
 // Swiperの名残　正直Swiperの方がテキストの充実という意味では使いやすいかも
-// import {Swiper,SwiperSlide} from "swiper/react";
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import {Navigation} from "swiper/modules";
+
+import {Swiper,SwiperSlide} from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import {Scrollbar} from "swiper/modules";
 
 // import Product from "@/app/product/page";
 // import Image from "next/image"
@@ -27,29 +29,20 @@ const Toppage_2nd = () => {
     return (
         <>
             <div className={"inner"}>
-                <Slide cssClass={"slide2"} arrows={false} slidesToShow={3} slidesToScroll={1} indicators={true}
-                       pauseOnHover={true}>
-                    <div className={"each-slide-effect"}>
-                        <div className={"boxes"} style={{'backgroundImage': `url(${images[0]})`}}>
-                        </div>
-                        <p className={"s2tit"}>ここにタイトルを入力</p>
-                    </div>
-                    <div className={"each-slide-effect"}>
-                        <div className={"boxes"} style={{'backgroundImage': `url(${images[1]})`}}>
-                        </div>
-                        <p className={"s2tit"}>スライド2_ここにタイトルを入力</p>
+                <Swiper
+                    modules={[Scrollbar,]}
+                    spaceBetween={50}
+                    slidesPerView={2}
+                    scrollbar={{draggable:true}}
+                >
+                    <SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[0]})`}}><p>slide1</p></div></SwiperSlide>
+                    <SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[1]})`}}><p>slide2</p></div></SwiperSlide>
+                    <SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[2]})`}}><p>slide3</p></div></SwiperSlide>
 
-                    </div>
+                </Swiper>
 
-                    <div className={"each-slide-effect"}>
-                        <div className={"boxes"} style={{'backgroundImage': `url(${images[2]})`}}>
-                        </div>
-                        <p className={"s2tit"}>スライド3_ここにタイトルを入力</p>
 
-                    </div>
 
-                </Slide>
-                {/*<div id={"S2under"}></div>*/}
 
             </div>
 
