8f03a75f47d13122737a795063473c0568f252cc
35018 <nhs35018@nhs.hal.ac.jp>
2024-11-01 17:15:03 +0900

    辞書型化

--- src/app/_components/Toppage_2nd/Toppage_2nd.tsx
+++ src/app/_components/Toppage_2nd/Toppage_2nd.tsx
@@ -2,42 +2,55 @@
 import React, {useState} from 'react';
 import "./common.css"
 
-// react-slideshow-image使う場合
-// import {Slide} from 'react-slideshow-image';
-// import 'react-slideshow-image/dist/styles.css'
-
-// Swiperの名残　正直Swiperの方がテキストの充実という意味では使いやすいかも
 
 import {Swiper,SwiperSlide} from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/scrollbar';
-import {Scrollbar} from "swiper/modules";
+import 'swiper/css/pagination';
+import {Autoplay,Scrollbar,Pagination} from "swiper/modules";
 
 // import Product from "@/app/product/page";
 // import Image from "next/image"
 // import Link from "next/link";
 
+// Active判定で違うIDの値を返す
 
 
 const Toppage_2nd = () => {
-    const images = [
-        '../images/Slide/slide_color.png',
-        '../images/Slide/slide_color.png',
-        '../images/Slide/slide_color.png',
+    // スライドの中身の辞書
+    const Slide2dex = [
+        {img:'../images/Slide/slide_color.png',
+        tit:'first'},
+        {img:'../images/Slide/slide_color.png',
+            tit:'2nd'},
+        {img:'../images/Slide/slide_color.png',
+            tit:'third'},
     ];
+
     return (
         <>
             <div className={"inner"}>
                 <Swiper
-                    modules={[Scrollbar,]}
+                    modules={[Autoplay,Scrollbar,Pagination]}
                     spaceBetween={50}
-                    slidesPerView={2}
+                    slidesPerView={3}
+                    centeredSlides={true}
+                    autoplay={true}
+                    loop={true}
+                    followFinger={false}
                     scrollbar={{draggable:true}}
+                    pagination={{el:".swiper-pagination", clickable:true}}
                 >
-                    <SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[0]})`}}><p>slide1</p></div></SwiperSlide>
-                    <SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[1]})`}}><p>slide2</p></div></SwiperSlide>
-                    <SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[2]})`}}><p>slide3</p></div></SwiperSlide>
+                    <SwiperSlide>
+                        {({isActive}) => (<div>Current slide is ?</div>)}
+                    </SwiperSlide>
+                    <SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${Slide2dex[0].img})`}}><p className={"s2tit"}>{Slide2dex[0].tit}</p></div></SwiperSlide>
+                    <SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${Slide2dex[1].img})`}}><p className={"s2tit"}>{Slide2dex[1].tit}</p></div></SwiperSlide>
+                    {/*<SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[2]})`}}><p className={"s2tit"}>slide3</p></div></SwiperSlide>*/}
+                    {/*<SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[2]})`}}><p className={"s2tit"}>slide4</p></div></SwiperSlide>*/}
+                    {/*<SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[2]})`}}><p className={"s2tit"}>slide5</p></div></SwiperSlide>*/}
+                    {/*<SwiperSlide><div className={"boxes"} style={{'backgroundImage': `url(${images[2]})`}}><p className={"s2tit"}>slide6</p></div></SwiperSlide>*/}
 
                 </Swiper>
 
