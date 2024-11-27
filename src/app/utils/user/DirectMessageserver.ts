b3a2e4d49657f2ab90e07725f2abda9a63b87f96
hayasin2004 <masataka1kousuke1@gmail.com>
2024-11-28 06:02:40 +0900

    リアタイムでDMをするために2つライブラリを入れました。 express　→　Apiのやり取りを簡単にしてくれる soket.io →　リアルタイムのやり取りをやるため

--- src/app/utils/user/DirectMessageserver.ts
+++ src/app/utils/user/DirectMessageserver.ts
@@ -3,18 +3,31 @@
 import {string} from "prop-types";
 import {User} from "@/models/User";
 import {connectDB} from "@/lib/mongodb";
+import {MongoClient} from "mongodb";
+import {Chat} from "@/models/Chat";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server  = http.createServer(app)
+const io = new Server(server)
 
 const DirectMessageserver = async (detailUser?: string, currentUser?: string) => {
     await connectDB()
-
-
-
+    const uri = process.env.MONGODB_URI!;
+    const client = new MongoClient(uri)
     try {
+        const database = client.db("Chat");
+        const collection = database.collection("currentUser");
+        const changeStream = collection.watch()
+        changeStream.on("change" , (change) => {
+            console.log("change", change);
+        })
         console.log("erxtcyvugbijomkp" + currentUser)
         const partnerUserData = await User.findById({_id: detailUser});
         const currentUserData = await User.findById({_id: currentUser});
-        console.log("対象のユーザー" + partnerUserData);
-        console.log("ログインしているユーザー" + currentUserData);
+        // console.log("対象のユーザー" + partnerUserData);
+        // console.log("ログインしているユーザー" + currentUserData);
     } catch (err) {
         console.log(err)
     }
---
+++ src/models/Chat.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema({
+
+    currentUser : {
+        type: String,
+        required: true,
+    },
+    partnerUser : {
+        type: String,
+        required: true,
+    },
+    currentUserChat :{
+        type : Array,
+        default: [],
+    },
+    partnerUserChat : {
+        type : Array ,
+        default: [],
+    }
+
+})
+
+export const Chat = mongoose.model("Chat", ChatSchema)
