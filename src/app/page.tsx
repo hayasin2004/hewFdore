8e7ff167f2059849179b810540318b5ba85aedb7
hayasin2004 <masataka1kousuke1@gmail.com>
2024-12-16 06:07:44 +0900

    商品追加されたときに通知が来るような機能作成します。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,11 @@ import Toppage from "@/app/toppage/page";
 import Gmail from "./_components/gmail/Gmail";
 import SearchPageProducts from "@/app/searchResult/page";
 import SearchProduct from "@/app/_components/(search)/searchProduct/Searchproduct";
+import {MongoClient} from "mongodb";
 
 export default function Home() {
-  return (
+
+    return (
     <main>
       <div>
           <SearchProduct/>
