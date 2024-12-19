e84e4d6b0bd4e50b9186d5e5852aa7068a2b2215
Reo <reo1028m@gmail.com>
2024-12-19 11:38:03 +0900

    cms

---
+++ src/app/api/create-payment-intent/route.ts
@@ -0,0 +1,36 @@
+// This is your test secret API key.
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const calculateOrderAmount = () => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    return 1400;
+};
+
+export default async function handler(req: { body: { items: any; }; }, res: {
+        send: (arg0: {
+            clientSecret: any;
+            // [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
+            dpmCheckerLink: string;
+        }) => void;
+    }) {
+    const { items } = req.body;
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(),
+        currency: "eur",
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+        // [DEV]: For demo purposes only, you should avoid exposing the PaymentIntent ID in the client-side code.
+        dpmCheckerLink: `https://dashboard.stripe.com/settings/payment_methods/review?transaction_id=${paymentIntent.id}`,
+    });
+
+};
