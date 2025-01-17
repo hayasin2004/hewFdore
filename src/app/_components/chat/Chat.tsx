7e6e453abca3ac9a36251917648fe779bf67a00e
saito <nhs30526@nhs.hal.ac.jp>
2025-01-17 19:00:08 +0900

    取引コメントのデザインを修正

--- src/app/_components/chat/Chat.tsx
+++ src/app/_components/chat/Chat.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 import React, {useEffect, useState} from 'react';
 import Image from "next/image";
@@ -63,42 +64,60 @@ const Chat = (props: { paramsProductData: string }) => {
 
     return (
         <>
-            <p>出品者の投稿</p>
-            {listingChatMessageList?.map((item, index) => (
-                <ul key={index}>
-                    質問者Id : {item.senderUserId} <br/>
-                    質問者名前 : {item.listingMessage} <br/>
-                    メッセージ内容 : {item?._id} <br/>
-                    <button onClick={() => testCommentLike(item?._id)}>
-                        いいね : {item?.buyerMessageLike?.length}
+            <div className={"comment-areaFrame"}>
+                <p>出品者の投稿</p>
+                {listingChatMessageList?.map((item, index) => (
+                    <div className={"comment-sec"}>
+                        <div className={"comment-user"}>
+                            <Images
+                                src={"/images/sampleIcon.jpg"} style={{borderRadius: "50px"}} width={50} height={50}
+                                alt={"サンプルユーザーアイコン"}/>
+                            {item.listingMessage} さん<br/>
+                        </div>
+                        <div className={"comment-area"} key={index}>
+                            {/*質問者Id : {item.senderUserId} <br/>*/}
+
+                            メッセージ内容 : {item?._id} <br/>
+                            <button id={"good"} onClick={() => testCommentLike(item?._id)}>
+                                ♡{item?.buyerMessageLike?.length}
+                            </button>
+                        </div>
+                    </div>
+
+                ))}
+
+
+                <p>閲覧者の投稿</p>
+                {buyerChatMessageList?.map((item, index) => (
+                    <ul key={index}>
+                        {item.senderUserId} <br/>
+                        {item.buyerMessage} さん<br/>
+                        メッセージ内容 : {item?._id} <br/>
+                        <button onClick={() => testCommentLike(item?._id)}>
+                            ♡{item?.buyerMessageLike?.length}
+                        </button>
+                    </ul>
+                ))}
+                {props.paramsProductData}
+
+                <div className="Productchat">
+                    <Images id={"usericon"}
+                            src={"/images/sampleIcon.jpg"} style={{borderRadius: "50px"}} width={50} height={50}
+                            alt={"サンプルユーザーアイコン"}/>
+
+                    <label htmlFor="msg" style={{display: "none"}}>問い合わせフォーム</label>
+
+                    <textarea name="msg" id="msg" onChange={(e) => {
+                        setChatMessage(e.target.value)
+                    }} placeholder="出品者へのお問い合わせはこちらから"/>
+                    {/*<input type="submit" formTarget={"msg"}/>*/}
+                    <button onClick={submitChatMessage} type={"submit"}>
+                        <img id={"sendMsg"} height={30} src={"/images/mail_1.svg"} width={30}/>
                     </button>
-                </ul>
-            ))}
-            <p>閲覧者の投稿</p>
-            {buyerChatMessageList?.map((item, index) => (
-                <ul key={index}>
-                    質問者Id : {item.senderUserId} <br/>
-                    質問者名前 : {item.buyerMessage} <br/>
-                    メッセージ内容 : {item?._id} <br/>
-                    <button onClick={() => testCommentLike(item?._id)}>
-                        いいね : {item?.buyerMessageLike?.length}
-                    </button>
-                </ul>
-            ))}
-            {props.paramsProductData}
-            <div className="Productchat">
-                <Images
-                    src={"/images/sampleIcon.jpg"} style={{borderRadius: "50px"}} width={50} height={50}
-                    alt={"サンプルユーザーアイコン"}/>
-                <label htmlFor="msg" style={{display: "none"}}>問い合わせフォーム</label>
-                <input type="text" name="msg" id="msg" onChange={(e) => {
-                    setChatMessage(e.target.value)
-                }} placeholder="出品者へのお問い合わせはこちらから"/>
-                {/*<input type="submit" formTarget={"msg"}/>*/}
-                <button onClick={submitChatMessage} type={"submit"}>
-                    <img id={"sendMsg"} height={30} src={"/images/mail_1.svg"} width={30}/>
-                </button>
+                </div>
             </div>
+
+
         </>
 
     );
