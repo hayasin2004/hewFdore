34c099baaba80fef0c2ca522ae17508d622b9183
hayasin2004 <masataka1kousuke1@gmail.com>
2025-01-21 07:42:13 +0900

    購入者が評価したら通知が来るようになりました

--- src/app/utils/product/tradeEnd.ts
+++ src/app/utils/product/tradeEnd.ts
@@ -11,13 +11,13 @@ const tradeEnd = async (purchaseId: string | null, status: string | null, curren
         // tradeStatus :0→既に取引終了していたがバグによる入力 1→購入者評価後に購入者評価待ち , 2→購入者評価後取引終了 , 3→購入者評価後に購入者評価待ち 4→購入者評価後取引終了 5→既に評価送信済み
         const purchaseCondition = await Purchase.findById(purchaseId).select("productId sellerId buyerId sellerUserLastChat sellerUserLastReview buyerUserLastChat buyerUserLastReview")
         console.log(purchaseCondition.sellerUserLastChat !== "" && purchaseCondition.sellerUserLastReview !== "")
-        if (purchaseCondition.sellerUserLastChat !== "" && purchaseCondition.sellerUserLastReview !== "" && purchaseCondition.buyerUserLastChat !== "" && purchaseCondition.buyerUserReview !== "") {
+        if (purchaseCondition.sellerUserLastChat !== "" && purchaseCondition.sellerUserLastReview !== "" && purchaseCondition.buyerUserLastChat !== "" && purchaseCondition.buyerUserLastReview !== "") {
             console.log("既に出品者、購入者の両者から評価をもらっています。")
             const lastChatReview = {
                 sellerUserLastChat: purchaseCondition.sellerUserLastChat,
                 sellerUserLastReview: purchaseCondition.sellerUserLastReview,
                 buyerUserLastChat: purchaseCondition.buyerUserLastChat,
-                buyerUserReview: purchaseCondition.buyerUserReview
+                buyerUserLastReview: purchaseCondition.buyerUserLastReview
             }
             tradeStatus = 0;
             return {tradeStatus: tradeStatus, lastChatReview: JSON.stringify(lastChatReview)}
@@ -29,7 +29,7 @@ const tradeEnd = async (purchaseId: string | null, status: string | null, curren
                     sellerUserLastChat: purchaseCondition.sellerUserLastChat,
                     sellerUserLastReview: purchaseCondition.sellerUserLastReview,
                     buyerUserLastChat: purchaseCondition.buyerUserLastChat,
-                    buyerUserReview: purchaseCondition.buyerUserReview
+                    buyerUserLastReview: purchaseCondition.buyerUserLastReview
                 }
                 tradeStatus = 5;
                 return {tradeStatus: tradeStatus, lastChatReview: JSON.stringify(lastChatReview)}
@@ -43,7 +43,7 @@ const tradeEnd = async (purchaseId: string | null, status: string | null, curren
                     }, {new: true}
                 )
                 console.log(sellerPurchaseLastUpdate)
-                if (sellerPurchaseLastUpdate.buyerUserLastChat == "" || sellerPurchaseLastUpdate.buyerUserReview == "") {
+                if (sellerPurchaseLastUpdate.buyerUserLastChat == "" || sellerPurchaseLastUpdate.buyerUserLastReview == "") {
                     console.log("出品者評価後、購入者の両者から評価を待ちます。")
                     tradeStatus = 1;
                     const lastChatReview = {
@@ -51,14 +51,15 @@ const tradeEnd = async (purchaseId: string | null, status: string | null, curren
                         sellerUserLastChat: sellerPurchaseLastUpdate.sellerUserLastChat,
                         sellerUserLastReview: sellerPurchaseLastUpdate.sellerUserLastReview,
                         buyerUserLastChat: sellerPurchaseLastUpdate.buyerUserLastChat,
-                        buyerUserReview: sellerPurchaseLastUpdate.buyerUserReview
+                        buyerUserLastReview: sellerPurchaseLastUpdate.buyerUserLastReview
                     }
                     // 出品者の評価→通知するのは購入者のベル
-                    await toastPurchaseReview(purchaseCondition?.buyerId,purchaseCondition?.productId ,lastMessage , reviewValue )
+                    await toastPurchaseReview(purchaseCondition?.sellerId,purchaseCondition?.buyerId, purchaseCondition?.productId, lastMessage, reviewValue)
                     return {tradeStatus: tradeStatus, lastChatReview: JSON.stringify(lastChatReview)}
 
+
                 }
-                if (sellerPurchaseLastUpdate.sellerUserLastChat !== "" && sellerPurchaseLastUpdate.sellerUserLastReview !== "" && sellerPurchaseLastUpdate.buyerUserLastChat !== "" && sellerPurchaseLastUpdate.buyerUserReview !== "") {
+                if (sellerPurchaseLastUpdate.sellerUserLastChat !== "" && sellerPurchaseLastUpdate.sellerUserLastReview !== "" && sellerPurchaseLastUpdate.buyerUserLastChat !== "" && sellerPurchaseLastUpdate.buyerUserLastReview !== "") {
                     console.log("出品者評価後、購入者の両者から評価を貰いました。")
                     const tradeStatusUpdate = await sellerPurchaseLastUpdate.updateOne({
                             $set: {
@@ -71,12 +72,13 @@ const tradeEnd = async (purchaseId: string | null, status: string | null, curren
                         sellerUserLastChat: tradeStatusUpdate.sellerUserLastChat,
                         sellerUserLastReview: tradeStatusUpdate.sellerUserLastReview,
                         buyerUserLastChat: tradeStatusUpdate.buyerUserLastChat,
-                        buyerUserReview: tradeStatusUpdate.buyerUserReview
+                        buyerUserLastReview: tradeStatusUpdate.buyerUserLastReview
                     }
                     const sellerUserLastChat = sellerPurchaseLastUpdate.sellerUserLastChat
 
                     console.log(lastChatReview)
                     tradeStatus = 2;
+                    await toastPurchaseReview(purchaseCondition?.sellerId,purchaseCondition?.buyerId, purchaseCondition?.productId, lastMessage, reviewValue)
                     return {tradeStatus: tradeStatus, lastChatReview: JSON.stringify(sellerUserLastChat)}
                 }
 
@@ -85,14 +87,14 @@ const tradeEnd = async (purchaseId: string | null, status: string | null, curren
         }
 
         if (status === "2") {
-            if (purchaseCondition.buyerUserLastChat !== "" || purchaseCondition.buyerUserReview !== "") {
+            if (purchaseCondition.buyerUserLastChat !== "" || purchaseCondition.buyerUserLastReview !== "") {
                 console.log("既に最終評価しています。")
 
                 const lastChatReview = {
                     sellerUserLastChat: purchaseCondition.sellerUserLastChat,
                     sellerUserLastReview: purchaseCondition.sellerUserLastReview,
                     buyerUserLastChat: purchaseCondition.buyerUserLastChat,
-                    buyerUserReview: purchaseCondition.buyerUserReview
+                    buyerUserLastReview: purchaseCondition.buyerUserLastReview
                 }
                 tradeStatus = 5;
                 return {tradeStatus: tradeStatus, lastChatReview: JSON.stringify(lastChatReview)}
@@ -103,27 +105,30 @@ const tradeEnd = async (purchaseId: string | null, status: string | null, curren
                 const buyerPurchaseLastUpdate = await Purchase.findByIdAndUpdate({_id: purchaseId}, {
                         $set: {
                             buyerUserLastChat: lastMessage,
-                            buyerUserReview: reviewValue,
+                            buyerUserLastReview: reviewValue,
                         }
                     }, {new: true}
                 )
-                console.log(buyerPurchaseLastUpdate)
-
-                if (buyerPurchaseLastUpdate.buyerUserLastChat == "" || buyerPurchaseLastUpdate.buyerUserReview == "") {
+                if (buyerPurchaseLastUpdate.listingUserLastChat == "" || buyerPurchaseLastUpdate.listingUserLastReview == "") {
                     console.log("購入者評価後、出品者の両者から評価を待ちます。")
+                    console.log("出品者評価")
+
                     const lastChatReview = {
                         sellerUserLastChat: buyerPurchaseLastUpdate.sellerUserLastChat,
                         sellerUserLastReview: buyerPurchaseLastUpdate.sellerUserLastReview,
                         buyerUserLastChat: buyerPurchaseLastUpdate.buyerUserLastChat,
-                        buyerUserReview: buyerPurchaseLastUpdate.buyerUserReview
+                        buyerUserLastReview: buyerPurchaseLastUpdate.buyerUserLastReview
                     }
                     tradeStatus = 3;
+                    await toastPurchaseReview(purchaseCondition?.buyerId , purchaseCondition?.sellerId, purchaseCondition?.productId, lastMessage, reviewValue)
                     return {tradeStatus: tradeStatus, lastChatReview: JSON.stringify(lastChatReview)}
 
 
                 }
-                if (buyerPurchaseLastUpdate.sellerUserLastChat !== "" && buyerPurchaseLastUpdate.sellerUserLastReview !== "" && buyerPurchaseLastUpdate.buyerUserLastChat !== "" && buyerPurchaseLastUpdate.buyerUserReview !== "") {
+                if (buyerPurchaseLastUpdate.sellerUserLastChat !== "" && buyerPurchaseLastUpdate.sellerUserLastReview !== "" && buyerPurchaseLastUpdate.buyerUserLastChat !== "" && buyerPurchaseLastUpdate.buyerUserLastReview !== "") {
                     console.log("購入者の評価後、出品者の両者から評価がそろいました。")
+                    console.log("出品者評価")
+
                     const tradeStatusUpdate = await buyerPurchaseLastUpdate.updateOne({
                             $set: {
                                 tradeStatus: "取引終了"
@@ -135,9 +140,10 @@ const tradeEnd = async (purchaseId: string | null, status: string | null, curren
                         sellerUserLastChat: tradeStatusUpdate.sellerUserLastChat,
                         sellerUserLastReview: tradeStatusUpdate.sellerUserLastReview,
                         buyerUserLastChat: tradeStatusUpdate.buyerUserLastChat,
-                        buyerUserReview: tradeStatusUpdate.buyerUserReview
+                        buyerUserLastReview: tradeStatusUpdate.buyerUserLastReview
                     }
                     tradeStatus = 4;
+                    await toastPurchaseReview(purchaseCondition?.buyerId,purchaseCondition?.sellerId, purchaseCondition?.productId, lastMessage, reviewValue)
                     return {tradeStatus: tradeStatus, lastChatReview: JSON.stringify(lastChatReview)}
 
                 }
--- src/app/utils/toast/toastPurchaseReview.ts
+++ src/app/utils/toast/toastPurchaseReview.ts
@@ -8,19 +8,20 @@ import {Product} from "@/models/Product";
 import {ProductType} from "@/app/utils/product/productDetail";
 
 // 通知を追加する機能
-const toastPurchaseReview = async  (currentUserId : string | null ,productId : string | null ,lastMessage : string | null , reviewValue : number | null ) => {
+const toastPurchaseReview = async  (currentUserId : string | null ,partnerUserId : string | null ,productId : string | null ,lastMessage : string | null , reviewValue : number | null ) => {
 // const toastPurchaseReview = async  () => {
     await connectDB()
     console.log("currentUserId" + currentUserId , "productId" + productId , "lastMessage" + lastMessage , "reviewValue" + reviewValue )
     try {
         const user : UserType | null = await User.findOne({_id : currentUserId})
+        const partnerUser : UserType | null = await User.findOne({_id : partnerUserId})
         const product : ProductType | null = await Product.findById({_id : productId})
         const purchase = {
-            userId : user?._id,
+            userId : partnerUser?._id,
             productId : product?._id,
             productName : product?.productName,
-            message : `商品「${product?.productName}」が評価されました！評価メッセージ ${lastMessage} , 評価 ${reviewValue} `,
-            toastCategory : "いいね、フォローなど",
+            message : `${user?.username}が商品「${product?.productName}」を評価しました！評価メッセージ ${lastMessage} , 評価 ${reviewValue} `,
+            toastCategory : "商品関連",
             alreadyRead : "未読",
         }
 
