63490a0e4915cbe8f46bb7989ccd2b52aaa8085b
Reo <reo1028m@gmail.com>
2025-01-23 11:20:30 +0900

    cms

--- src/app/_components/blog/Blogintroduction.tsx
+++ src/app/_components/blog/Blogintroduction.tsx
@@ -3,19 +3,23 @@
 import React, { useEffect, useState } from "react";
 import { GetBlog } from "@/lib/client";
 import Link from "next/link";
-import "./Blogintroduction.css"
+import "./Blogintroduction.css";
 
 export interface BlogType {
     id?: string;
     title?: string;
     contents?: string;
+    image?: {
+        url: string;
+        height: number;
+        width: number;
+    };
 }
 
 const Blogintroduction = () => {
     const [blogs, setBlogs] = useState<BlogType[]>([]);
 
     useEffect(() => {
-
         const fetchBlogs = async () => {
             const data = await GetBlog(); // データ取得
             console.log("取得したデータ:", data);
@@ -32,13 +36,23 @@ const Blogintroduction = () => {
             ) : (
                 <ul>
                     {blogs.map((blog) => (
-                        <li key={blog.id}>
-                            {/*各ブログの詳細ページのLink*/}
-
-                            <Link href={`/${blog.id}`}>
-                            <div className={"linkid"}>
-                                {blog.title}
-                            </div>
+                        <li key={blog.id} style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+                            {/* 各ブログの詳細ページのLink */}
+                            <Link href={`/${blog.id}`} style={{ display: "flex", alignItems: "center", textDecoration: "none", color: "inherit" }}>
+                                {/* 画像表示 */}
+                                {blog.image && (
+                                    <img
+                                        src={blog.image.url}
+                                        alt={blog.title}
+                                        style={{
+                                            width: "50px",
+                                            height: "auto",
+                                            marginRight: "10px",
+                                            borderRadius: "5px",
+                                        }}
+                                    />
+                                )}
+                                <div className={"linkid"}>{blog.title}</div>
                             </Link>
                         </li>
                     ))}
@@ -49,6 +63,67 @@ const Blogintroduction = () => {
 };
 
 export default Blogintroduction;
+
+
+
+
+
+
+
+//修正前(最新)
+
+// "use client";
+//
+// import React, { useEffect, useState } from "react";
+// import { GetBlog } from "@/lib/client";
+// import Link from "next/link";
+// import "./Blogintroduction.css"
+//
+// export interface BlogType {
+//     id?: string;
+//     title?: string;
+//     contents?: string;
+//
+// }
+//
+// const Blogintroduction = () => {
+//     const [blogs, setBlogs] = useState<BlogType[]>([]);
+//
+//     useEffect(() => {
+//
+//         const fetchBlogs = async () => {
+//             const data = await GetBlog(); // データ取得
+//             console.log("取得したデータ:", data);
+//             setBlogs(data);
+//         };
+//         fetchBlogs();
+//     }, []);
+//
+//     return (
+//         <div>
+//             <h1>ブログ一覧</h1>
+//             {blogs.length === 0 ? (
+//                 <p>読み込み中...</p>
+//             ) : (
+//                 <ul>
+//                     {blogs.map((blog) => (
+//                         <li key={blog.id}>
+//                             {/*各ブログの詳細ページのLink*/}
+//
+//                             <Link href={`/${blog.id}`}>
+//                             <div className={"linkid"}>
+//                                 {blog.title}
+//                             </div>
+//                             </Link>
+//                         </li>
+//                     ))}
+//                 </ul>
+//             )}
+//         </div>
+//     );
+// };
+//
+// export default Blogintroduction;
 
 
 
