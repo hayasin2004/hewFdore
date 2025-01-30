f444f7b6857a35e5190c090dbf19a4c99b7569b9
35018 <nhs35018@nhs.hal.ac.jp>
2025-01-30 11:59:55 +0900

    confirmPointsへのリンク削除:svgはsvgタグで入れると色変えできて、imgで入れるとcolorの影響受けなくなる

--- src/app/(confirm)/confirmUser/page.tsx
+++ src/app/(confirm)/confirmUser/page.tsx
@@ -13,23 +13,23 @@ const ConfirmUserProfile = () => {
         <main id={"CUmain"}>
             <div id={"confirms"}>
 
-                <Link href={"confirmPoints"}>
-                    <div className={"confirm"}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                             className="lucide lucide-badge-russian-ruble">
-                            <path
-                                d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/>
-                            <path d="M9 16h5"/>
-                            <path d="M9 12h5a2 2 0 1 0 0-4h-3v9"/>
-                        </svg>
-                        <div className={"confirm_text"}>
-                            <p>現在の保持ポイント</p>
-                            <p id={"getPoint"}>10pt</p>
-                            <p id={"pointHst"}>獲得履歴を見る</p>
-                        </div>
-                    </div>
-                </Link>
+                {/*<Link href={"confirmPoints"}>*/}
+                {/*    <div className={"confirm"}>*/}
+                {/*        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" fill="none"*/}
+                {/*             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"*/}
+                {/*             className="lucide lucide-badge-russian-ruble">*/}
+                {/*            <path*/}
+                {/*                d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/>*/}
+                {/*            <path d="M9 16h5"/>*/}
+                {/*            <path d="M9 12h5a2 2 0 1 0 0-4h-3v9"/>*/}
+                {/*        </svg>*/}
+                {/*        <div className={"confirm_text"}>*/}
+                {/*            <p>現在の保持ポイント</p>*/}
+                {/*            <p id={"getPoint"}>10pt</p>*/}
+                {/*            <p id={"pointHst"}>獲得履歴を見る</p>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</Link>*/}
 
                 <Link href={"favorite"}>
                     <div className={"confirm"} id={"fav"}>
