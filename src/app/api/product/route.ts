885ec34b58ffac94919c1aa957419e7a59535ff6
hayasin2004 <masataka1kousuke1@gmail.com>
2025-02-04 08:48:09 +0900

    デプロイ修正

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,7 +1,6 @@
 import {Product} from "@/models/Product";
 import {connectDB} from "@/lib/mongodb";
-import {NextApiRequest } from "next";
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 
 export interface DBProductType {
     _id?: string;
@@ -12,19 +11,16 @@ export interface DBProductType {
 }
 
 
-export async function GET(req: NextApiRequest, res: NextResponse) {
+export async function GET(req: NextRequest ) {
     await connectDB()
-
 
     if (req.method === "GET") {
         try {
             const product = await Product.find({sellStatus : "selling"});
-            console.log(res)
             return NextResponse.json(product)
         } catch (err) {
             console.log(err)
              return NextResponse.json({status: "Error", message: "商品の取得に失敗"})
-
         }
     } else {
          return NextResponse.json({status: "Error", message: "メソッドが違うかも"})
