0d7a0430e090b5d9796c4b982e2c746fd2b8b863
souta <soukaiani0901@gmail.com>
2025-02-20 11:57:57 +0900

    相手ユーザーのプロフィール画面　未完成

--- src/app/(user_communication_test)/userDetail/[id]/page.tsx
+++ src/app/(user_communication_test)/userDetail/[id]/page.tsx
@@ -10,6 +10,7 @@ import {ProductType} from "@/app/utils/product/productDetail";
 import CatchLikeList from "@/app/utils/user/CatchlikeList";
 import confirmUser from "@/app/utils/user/confirmUser";
 import Images from "next/image";
+import "./userDetail.css"
 
 
 const UserDetailPage = ({params}: { params: { id: UserType | null } }) => {
@@ -60,81 +61,103 @@ const UserDetailPage = ({params}: { params: { id: UserType | null } }) => {
         response()
     }, [token]);
 
+    const [selectedTab, setSelectedTab] = useState("tab1");
 
     return (
         <div>
-            <h1>
-                ObjectId: {params?.id}
-            </h1>
+            {/*<h1>*/}
+            {/*    ObjectId: {params?.id}*/}
+            {/*</h1>*/}
             <div>
-                <ul style={{display: "flex", flexDirection: "column"}}>
-                    <li>オブジェクトID: {userData?._id}</li>
-                    <li>ユーザーID: {userData?.userId}</li>
-                    <li>ユーザー名: {userData?.username}</li>
-                    <li>ユーザーメールアドレス: {userData?.email}</li>
-                    <li>ユーザー自己紹介: {userData?.desc}</li>
-                    <li>背景画像１:<Images src={userData?.profilePicture} alt={"ユーザーのプロフィール画像"} width={100} height={100}/> </li>
-                    <li>背景画像２: {userData?.coverProfilePicture}</li>
+                <div className={"partnerProfile"}>
+                    <div className={"partnerProfile img"}><Images src={userData?.profilePicture} alt={"ユーザーのプロフィール画像"} width={100}
+                                           height={100}/></div>
+                    {/*<li>オブジェクトID: {userData?._id}</li>*/}
+                    <div className={"user"}>
+                        <div>ユーザー名: {userData?.username}</div>
+                        <div>ユーザーID: {userData?.userId}</div>
+                    </div>
 
+                    {/*<li>ユーザーメールアドレス: {userData?.email}</li>*/}
 
-                    <p>いいねリスト</p>
-                    {/*{likeList?.likeList}*/}
-                    {likeList?.map((likeItem) => (
-                        <ul key={likeItem}>
-                            <Link href={`/product/${likeItem}`}>
-                                <li>{likeItem}</li>
-                            </Link>
-                        </ul>
-                    ))}
+                    {/*<li>背景画像２: {userData?.coverProfilePicture}</li>*/}
 
-                    <li>フォロワー一覧:
-                        フォロー中 : {userData?.followers?.map((item) => (
-                            <span key={item?._id}>
-                            <Link href={`/${item?._id}`}><p>{item?._id}</p></Link>
-                            </span>
-                        ))}
-                        <button onClick={() => followings()}>
-                            フォローする
-                        </button>
-                    </li>
-                    フォロワー: {userData?.followers?.map((item) => (
-                    <span key={item?._id}>
-                            <p>{item?._id}</p>
-                            </span>
-                ))}
+                    {/*<p>いいねリスト</p>*/}
+                    {/*/!*{likeList?.likeList}*!/*/}
+                    {/*{likeList?.map((likeItem) => (*/}
+                    {/*    <ul key={likeItem}>*/}
+                    {/*        <Link href={`/product/${likeItem}`}>*/}
+                    {/*            <li>{likeItem}</li>*/}
+                    {/*        </Link>*/}
+                    {/*    </ul>*/}
+                    {/*))}*/}
 
-                    <li>
-                        <Link href={{pathname: `/directMessage/${params.id}`, query: {currentUserId: loginUserData?._id}}}>
-                            DMする
-                        </Link>
-                    </li>
-                </ul>
-                <div>
-                    <h3>このユーザーが出品している商品</h3>
                     <div>
+                        <Link href={{
+                            pathname: `/directMessage/${params.id}`,
+                            query: {currentUserId: loginUserData?._id}
+                        }}>
+                            DM
+                        </Link>
+                    </div>
+
+                    <button onClick={() => followings()}>
+                        フォロー
+                    </button>
+                </div>
+
+
+
+                <div className={"selfIntroduction"}>
+                    自己紹介: {userData?.desc}
+                </div>
+
+
+                <div className={"tab_container"}>
+
+                    <input id="tab1" type="radio" name="tab_item" defaultChecked={selectedTab === "tab1"}
+                           onChange={() => setSelectedTab("tab1")}/>
+                    <label className="tab_item" htmlFor="tab1">このユーザーが出品している商品</label>
+
+                    <input id="tab2" type="radio" name="tab_item" defaultChecked={selectedTab === "tab2"}
+                           onChange={() => setSelectedTab("tab2")}/>
+                    <label className="tab_item" htmlFor="tab2">フォロー中</label>
+
+                    <input id="tab3" type="radio" name="tab_item" defaultChecked={selectedTab === "tab3"}
+                           onChange={() => setSelectedTab("tab3")}/>
+                    <label className="tab_item" htmlFor="tab3">フォロワー</label>
+
+                    <div className="tab_content" id="tab1_content">
                         {productData?.map((item: ProductType) => (
-                            <ul key={item?._id}>
+                            <div key={item?._id}>
                                 <br/>
-                                <li>商品名 : {item.productName}</li>
-                                <li>商品価格 : {item.productPrice}</li>
-                                <li>送料負担 : {item.postageBurden}</li>
-                                <li>商品カテゴリー : {item.productCategory}</li>
+                                <div>商品名 : {item.productName}</div>
+                                <div>商品価格 : {item.productPrice}</div>
+                                <div>送料負担 : {item.postageBurden}</div>
+                                <div>商品カテゴリー : {item.productCategory}</div>
                                 <Images src={item?.productImage ? item?.productImage : "/"} alt={"商品画像"} width={500} height={500} />
                                 <Link href={`/product/${item._id}`}>
-                                    <li>詳細を見る</li>
+                                    <div>詳細を見る</div>
                                 </Link>
                                 <br/>
-                                <hr/>
-                            </ul>
+                            </div>
                         ))}
+                    </div>
+
+                    <div className="tab_content" id="tab2_content">
+                        フォロー中のユーザーとってくる
+                    </div>
+
+                    <div className="tab_content" id="tab3_content">
+                        フォロワー一覧
                     </div>
                 </div>
 
-                <div>
-                    <p>ログインしている人</p>
-                    id : {loginUserData?._id} <br/>
-                    username : {loginUserData?.username}
-                </div>
+                {/*<div>*/}
+                {/*    <p>ログインしている人</p>*/}
+                {/*    id : {loginUserData?._id} <br/>*/}
+                {/*    username : {loginUserData?.username}*/}
+                {/*</div>*/}
 
             </div>
 
