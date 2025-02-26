df69082c3c603e9162971e4014d330b1f6e6f35d
hayasin2004 <masataka1kousuke1@gmail.com>
2025-02-27 05:47:55 +0900

    マジでこれをマージするのはやめたほうがいい！ ガチでぶちこわれてます

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
+"use client"
 import {withAuth} from "next-auth/middleware";
 import { NextResponse} from "next/server";
 
 export default withAuth(
     async function middleware(req) {
-        const token = await localStorage.getItem("token");
+        const token = localStorage.getItem("token");
         const isAuthPage =
             req.nextUrl.pathname.startsWith("/topppage") ||
             req.nextUrl.pathname.startsWith("/login")
